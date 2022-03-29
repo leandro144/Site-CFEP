72ed4e3ca95adcee665c59b89930e7c97ae42a2d
Leandro <73486095+leandro144@users.noreply.github.com>
2022-03-29 15:56:28 -0300

    index.html finalizado

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
-    ('window-scroll', window.scrollY > 100)
+    ('window-scroll', window.scrollY > 70)
 })
 
 // nav //
@@ -21,3 +21,24 @@ faqs.forEach(faq => {
         }
     })
 })
+
+
+// SHOW/HIDE //
+
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => (
+    menu.style.display = "flex",
+    closeBtn.style.display = "inline-block",
+    menuBtn.style.display = "none"
+))
+
+const closeNav = () => {
+    menu.style.display = "none",
+    closeBtn.style.display = "none",
+    menuBtn.style.display = "inline-block"
+}
+
+closeBtn.addEventListener('click', closeNav)
