002589f6b9e1f3994309d72994222f2d42da1242
LeandroCastro <leandro_castro980@hotmail.com>
2023-06-19 14:05:18 -0300

    atualizações

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle
-    ('window-scroll', window.scrollY > 70)
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 70)
 })
 
 // nav //
@@ -12,9 +11,9 @@ faqs.forEach(faq => {
         faq.classList.toggle('open');
 
         const icon = faq.querySelector('.faq__icon i');
-        if(icon.className === 'uil uil-plus') {
+        if (icon.className === 'uil uil-plus') {
             icon.className = "uil uil-minus";
-        }else{
+        } else {
             icon.className = 'uil uil-plus';
         }
     })
@@ -29,27 +28,63 @@ faqs.forEach(faq => {
 let navLinks = document.querySelector(".nav-links");
 let menuOpenBtn = document.querySelector(".navbar .bx-menu");
 let menuCloseBtn = document.querySelector(".nav-links .bx-x");
-menuOpenBtn.onclick = function() {
-navLinks.style.left = "0";
+menuOpenBtn.onclick = function () {
+    navLinks.style.left = "0";
 }
-menuCloseBtn.onclick = function() {
-navLinks.style.left = "-100%";
+menuCloseBtn.onclick = function () {
+    navLinks.style.left = "-100%";
 }
-
 
 // ****** MODAL ****** //
 
 const ethics = document.getElementById("btnethics")
+const btncode = document.getElementById("btncode")
 const img1 = document.getElementById("img1")
+const img2 = document.getElementById("img2")
 const backgroundModal = document.querySelector(".backgroundModal")
 const close = document.getElementById("close")
 
-ethics.addEventListener("click", () => {
-    img1.style.display = "block"
-    backgroundModal.style.display = "block"
-})
 
-close.addEventListener("click", () => {
-    img1.style.display = "none"
-    backgroundModal.style.display = "none"
-})
+let musicas = [
+    {titulo:'Guitar solo', artista:'João Tinti', source:'musicas/We Ride! - Reed Mathis.mp3', img:'imagens/rock.jpg'},
+    {titulo:'Samba raiz', artista:'Bossa Nova Brasil', source:'musicas/Ella Vater - The Mini Vandals.mp3', img:'imagens/samba.jpg'},
+    {titulo:'Música piano', artista:'John Green', source:'musicas/A Brand New Start - TrackTribe (1).mp3', img:'imagens/piano.jpg'}
+];
+
+// INICIO
+const chatLog = document.getElementById('chat-log');
+const userInput = document.getElementById('user-input');
+
+// Chatbot responses
+const botResponses = [
+  'Hello, how can I assist you?',
+  'What can I help you with today?',
+  'Feel free to ask me anything.'
+];
+
+// Function to add a message to the chat log
+function addMessageToLog(message, sender) {
+  const messageElement = document.createElement('div');
+  messageElement.classList.add('message');
+  messageElement.classList.add(sender);
+  messageElement.textContent = message;
+  chatLog.appendChild(messageElement);
+}
+
+// Function to handle user input
+function handleUserInput() {
+  const userMessage = userInput.value;
+  addMessageToLog(userMessage, 'user');
+  userInput.value = '';
+
+  // Get a random bot response
+  const randomResponse = botResponses[Math.floor(Math.random() * botResponses.length)];
+  addMessageToLog(randomResponse, 'bot');
+}
+
+// Event listener for user input
+userInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    handleUserInput();
+  }
+});
